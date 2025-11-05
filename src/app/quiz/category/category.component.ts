2d8406e14f50c4705011d1f8c155156670c28657
jimmy <lettevoueto.jimmy@gmail.com>
2025-11-05 11:15:53 +0100

    Ajout de l'input category

--- src/app/quiz/category/category.component.ts
+++ src/app/quiz/category/category.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-category',
   standalone: false,
   templateUrl: './category.component.html',
-  styleUrl: './category.component.scss'
+  styleUrl: './category.component.scss',
 })
 export class CategoryComponent {
+  category = '';
+  firstCategory = 'Fondamentaux d’Angular';
+  secondCategory = 'Développement et communication';
+  thirdCategory = 'Navigation et performance';
+  isCategoryConfirmed = false;
+  constructor(private router: Router) {}
 
+  confirmCategory() {
+    this.isCategoryConfirmed = true;
+    console.log(this.category);
+  }
 }
